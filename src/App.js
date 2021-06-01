b7a14bd42678c2611c80c65678742e0be0c6e9b7
Chanya Kukulprasong <chanjeedtu75@gmail.com>
2021-06-01 18:31:38 +0900

    react4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 /**
@@ -8,16 +8,14 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/dalmatian/cooper2.jpg',
+  )
   return [
     <header>
       <h1>Dogアプリ</h1>
     </header>,
     <p>犬の画像を表示するサイトです</p>,
-    <img
-      src="https://images.dog.ceo/breeds/dalmatian/cooper2.jpg"
-      alt="Dalmatian"
-      width="500"
-      height="300"
-    ></img>,
+    <img src={dogUrl} alt="Dalmatian" width="500" height="300"></img>,
   ]
 }
