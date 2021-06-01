c882c9eef28bf4eedfc395dd2476ccfb6737280e
Chanya Kukulprasong <chanjeedtu75@gmail.com>
2021-06-01 18:48:03 +0900

    react6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,25 @@ import './App.css'
  *
  * @type {React.FC}
  */
+
 export const App = () => {
+  const changeDogUrl = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(
+        result => {
+          console.log(result.message)
+          setDogUrl(result.message)
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          console.log('Error')
+          setDogUrl('https://images.dog.ceo/breeds/dalmatian/cooper2.jpg')
+        },
+      )
+  }
   const [dogUrl, setDogUrl] = useState(
     'https://images.dog.ceo/breeds/dalmatian/cooper2.jpg',
   )
@@ -19,14 +37,6 @@ export const App = () => {
     <img src={dogUrl} alt="Dalmatian" width="500" height="300"></img>,
     <br />,
     <br />,
-    <button
-      onClick={() =>
-        setDogUrl(
-          'https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg ',
-        )
-      }
-    >
-      更新
-    </button>,
+    <button onClick={() => changeDogUrl()}>更新</button>,
   ]
 }
